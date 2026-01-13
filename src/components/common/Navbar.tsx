6d460d5d82f6ae1b74d7bb2908bcb4557c2193a1
A-sleh <146457544+A-sleh@users.noreply.github.com>
2026-01-13 23:03:08 +0300

    fix bug in button icons

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -6,15 +6,15 @@ import { MdAddCard } from "react-icons/md";
 export const Navbar = () => {
   const [value, setValue] = useState("");
   return (
-    <div className="flex justify-between items-center p-2 w-full shadow-md">
+    <div className="flex justify-between items-center p-2 w-full shadow-md bg-white">
       <Search setValue={setValue} value={value} placeholder="البحث عن زبون" />
       <div className="flex gap-2 items-center">
         <button className="flex gap-2 items-center border-2 border-primary bg-primary text-white hover:bg-transparent hover:text-primary transition-all px-2 py-1 rounded-sm cursor-pointer">
-          <AiOutlineUserAdd size={22} />
+          <MdAddCard size={22} />
           <p className="hidden lg:block"> إضافة فاتورة</p>
         </button>
         <button className="flex gap-2 items-center border-2 border-primary text-primary hover:bg-primary hover:text-white transition-all px-2 py-1 rounded-sm cursor-pointer">
-          <MdAddCard size={22} />
+          <AiOutlineUserAdd size={22} />
           <p className="hidden lg:block"> إضافة زبون</p>
         </button>
       </div>
